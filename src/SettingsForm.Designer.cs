6a6cf294cd9f05444a7d2c9faec055d33706fbc1
AlexBond <AlexBondLeo@gmail.com>
2025-02-05 01:28:37 +0300

    SettingsForm

---
+++ src/SettingsForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace MHTextureManager
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            radioNew = new RadioButton();
+            groupBox1 = new GroupBox();
+            replaceOpen = new Button();
+            selectOpen = new Button();
+            createOpen = new Button();
+            replaceBox = new TextBox();
+            addBox = new TextBox();
+            createBox = new TextBox();
+            radioAdd = new RadioButton();
+            radioReplace = new RadioButton();
+            importButton = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // radioNew
+            // 
+            radioNew.AutoSize = true;
+            radioNew.Checked = true;
+            radioNew.Location = new Point(19, 24);
+            radioNew.Name = "radioNew";
+            radioNew.Size = new Size(49, 19);
+            radioNew.TabIndex = 0;
+            radioNew.TabStop = true;
+            radioNew.Text = "New";
+            radioNew.UseVisualStyleBackColor = true;
+            radioNew.CheckedChanged += radioNew_CheckedChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(replaceOpen);
+            groupBox1.Controls.Add(selectOpen);
+            groupBox1.Controls.Add(createOpen);
+            groupBox1.Controls.Add(replaceBox);
+            groupBox1.Controls.Add(addBox);
+            groupBox1.Controls.Add(createBox);
+            groupBox1.Controls.Add(radioAdd);
+            groupBox1.Controls.Add(radioReplace);
+            groupBox1.Controls.Add(radioNew);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(540, 115);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Import in TFC";
+            // 
+            // replaceOpen
+            // 
+            replaceOpen.Enabled = false;
+            replaceOpen.Location = new Point(451, 80);
+            replaceOpen.Name = "replaceOpen";
+            replaceOpen.Size = new Size(75, 23);
+            replaceOpen.TabIndex = 8;
+            replaceOpen.Text = "Replace...";
+            replaceOpen.UseVisualStyleBackColor = true;
+            replaceOpen.Click += replaceOpen_Click;
+            // 
+            // selectOpen
+            // 
+            selectOpen.Enabled = false;
+            selectOpen.Location = new Point(451, 51);
+            selectOpen.Name = "selectOpen";
+            selectOpen.Size = new Size(75, 23);
+            selectOpen.TabIndex = 7;
+            selectOpen.Text = "Select...";
+            selectOpen.UseVisualStyleBackColor = true;
+            selectOpen.Click += selectOpen_Click;
+            // 
+            // createOpen
+            // 
+            createOpen.Location = new Point(451, 22);
+            createOpen.Name = "createOpen";
+            createOpen.Size = new Size(75, 23);
+            createOpen.TabIndex = 6;
+            createOpen.Text = "Save as...";
+            createOpen.UseVisualStyleBackColor = true;
+            createOpen.Click += createOpen_Click;
+            // 
+            // replaceBox
+            // 
+            replaceBox.Enabled = false;
+            replaceBox.Location = new Point(91, 80);
+            replaceBox.Name = "replaceBox";
+            replaceBox.Size = new Size(354, 23);
+            replaceBox.TabIndex = 5;
+            // 
+            // addBox
+            // 
+            addBox.Enabled = false;
+            addBox.Location = new Point(91, 51);
+            addBox.Name = "addBox";
+            addBox.Size = new Size(354, 23);
+            addBox.TabIndex = 4;
+            // 
+            // createBox
+            // 
+            createBox.Enabled = false;
+            createBox.Location = new Point(91, 22);
+            createBox.Name = "createBox";
+            createBox.Size = new Size(354, 23);
+            createBox.TabIndex = 3;
+            // 
+            // radioAdd
+            // 
+            radioAdd.AutoSize = true;
+            radioAdd.Location = new Point(19, 53);
+            radioAdd.Name = "radioAdd";
+            radioAdd.Size = new Size(47, 19);
+            radioAdd.TabIndex = 1;
+            radioAdd.Text = "Add";
+            radioAdd.UseVisualStyleBackColor = true;
+            radioAdd.CheckedChanged += radioAdd_CheckedChanged;
+            // 
+            // radioReplace
+            // 
+            radioReplace.AutoSize = true;
+            radioReplace.Enabled = false;
+            radioReplace.Location = new Point(19, 82);
+            radioReplace.Name = "radioReplace";
+            radioReplace.Size = new Size(66, 19);
+            radioReplace.TabIndex = 2;
+            radioReplace.Text = "Replace";
+            radioReplace.UseVisualStyleBackColor = true;
+            radioReplace.CheckedChanged += radioReplace_CheckedChanged;
+            // 
+            // importButton
+            // 
+            importButton.DialogResult = DialogResult.OK;
+            importButton.Location = new Point(463, 133);
+            importButton.Name = "importButton";
+            importButton.Size = new Size(75, 23);
+            importButton.TabIndex = 3;
+            importButton.Text = "Import";
+            importButton.UseVisualStyleBackColor = true;
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(564, 162);
+            Controls.Add(importButton);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SettingsForm";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Import Texture Settings";
+            Load += SettingsForm_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RadioButton radioNew;
+        private GroupBox groupBox1;
+        private RadioButton radioAdd;
+        private RadioButton radioReplace;
+        private Button importButton;
+        private TextBox replaceBox;
+        private TextBox addBox;
+        private TextBox createBox;
+        private Button selectOpen;
+        private Button createOpen;
+        private Button replaceOpen;
+    }
+}
---
+++ src/SettingsForm.cs
@@ -0,0 +1,147 @@
+﻿
+namespace MHTextureManager
+{
+    public partial class SettingsForm : Form
+    {
+        public string TextureCachePath;
+        public string TextureCacheName;
+        public bool CanChanged;
+
+        public SettingsForm()
+        {
+            TextureCachePath = "";
+            TextureCacheName = "";
+            InitializeComponent();
+        }
+
+        private void CheckEnabled()
+        {
+            importButton.Enabled = false;
+            createOpen.Enabled = radioNew.Checked;
+            selectOpen.Enabled = radioAdd.Checked;
+            replaceOpen.Enabled = radioReplace.Checked;
+        }
+
+        private void radioNew_CheckedChanged(object sender, EventArgs e)
+        {
+            // Check New
+            CheckEnabled();
+            if (radioNew.Checked)
+                importButton.Enabled = checkFileName(createBox.Text, true);
+        }
+
+        private void radioAdd_CheckedChanged(object sender, EventArgs e)
+        {
+            // Check Add
+            CheckEnabled();
+            if (radioAdd.Checked)
+                importButton.Enabled = checkFileName(addBox.Text);
+        }
+
+        private void radioReplace_CheckedChanged(object sender, EventArgs e)
+        {
+            // Check Replace
+            CheckEnabled();
+            if (radioReplace.Checked)
+                importButton.Enabled = checkFileName(replaceBox.Text);
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            // Select possible States
+            radioNew.Checked = true;
+            importButton.Enabled = false;
+            replaceBox.Text = TextureCacheName;
+        }
+
+        private static string JustName(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                return string.Empty;
+
+            string fileName = Path.GetFileName(filePath);
+            return Path.GetFileNameWithoutExtension(fileName);
+        }
+
+        private bool checkFileName(string newChache, bool isNew = false)
+        {
+            if (newChache == string.Empty) return false;
+
+            string tfcPath = Path.Combine(TextureCachePath, newChache + ".tfc");
+            if (isNew && File.Exists(tfcPath))
+            {
+                MessageBox.Show($"Please select a new file. File {Path.GetFileName(tfcPath)} exist!",
+                                 "Invalid File", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (isNew == false && !File.Exists(tfcPath))
+            {
+                MessageBox.Show($"Please select an existing file. File {Path.GetFileName(tfcPath)} is not exist!",
+                                 "Invalid File", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (newChache == TextureCacheName)
+            {
+                MessageBox.Show($"{newChache} is already reserved for the standard tfc. Please choose another one!",
+                                 "Invalid File", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void createOpen_Click(object sender, EventArgs e)
+        {
+            using var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = TextureCachePath;
+            saveFileDialog.Filter = "TFC files (*.tfc)|*.tfc";
+            saveFileDialog.Title = "Select a new TFC file";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string newFile = saveFileDialog.FileName;
+                string newChache = JustName(newFile);
+
+                createBox.Text = newChache;
+                importButton.Enabled = checkFileName(newChache, true);
+            }
+        }
+
+        private void selectOpen_Click(object sender, EventArgs e)
+        {
+            using var selectFileDialog = new OpenFileDialog();
+            selectFileDialog.InitialDirectory = TextureCachePath;
+
+            selectFileDialog.Filter = "TFC files (*.tfc)|*.tfc";
+            selectFileDialog.Title = "Select a TFC file";
+
+            if (selectFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string newFile = selectFileDialog.FileName;
+                string newChache = JustName(newFile);
+
+                addBox.Text = newChache;
+                importButton.Enabled = checkFileName(newChache);
+            }
+        }
+
+        private void replaceOpen_Click(object sender, EventArgs e)
+        {
+            using var selectFileDialog = new OpenFileDialog();
+            selectFileDialog.InitialDirectory = TextureCachePath;
+            selectFileDialog.Filter = "TFC files (*.tfc)|*.tfc";
+            selectFileDialog.Title = "Select a TFC file";
+
+            if (selectFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string newFile = selectFileDialog.FileName;
+                string newChache = JustName(newFile);
+
+                replaceBox.Text = newChache;
+                importButton.Enabled = checkFileName(newChache);
+            }
+        }
+    }
+}
